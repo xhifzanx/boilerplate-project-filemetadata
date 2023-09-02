2ed314fd28a4ce1344d9a49a80fdc2f6569a891a
xhifzanx <88442980+xhifzanx@users.noreply.github.com>
2023-09-02 07:03:30 +0000

    fcc last node project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -7,14 +8,27 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const fs = require('fs');
 
+const uploadDirectory = 'uploads/';
 
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
